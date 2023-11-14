c958b018b26fe2a290a94768d8f419d7a7c90acb
nicolas-carabelli <108308637+nicolas-carabelli@users.noreply.github.com>
2023-11-14 10:14:35 -0300

    Create .eslintrc.js

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+  },
+  extends: 'eslint:recommended',
+  rules: {
+    'indent': ['error', 2],
+    'semi': ['error', 'always'],
+    'quotes': ['error', 'single'],
+    // Otras reglas aquí
+  },
+};
