46ebeda0aea1b47079169fdb8e73da4d20729071
nicolas-carabelli <108308637+nicolas-carabelli@users.noreply.github.com>
2023-11-24 18:58:15 -0300

    Delete funcion_lambda.js

--- funcion_lambda.js
+++
@@ -1,8 +0,0 @@
-exports.handler = async (event) => {
-    const mensaje = 'Hola desde mi función Lambda';  
-    const response = {
-        statusCode: 200,
-        body: JSON.stringify({ message: mensaje }),
-    };
-    return response;
-};
