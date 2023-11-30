aeb0c489d973e30cca32b6798788ed2ce124464d
nicolas-carabelli <108308637+nicolas-carabelli@users.noreply.github.com>
2023-11-30 14:49:07 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 exports.handler = async (event) => {
     console.log('Evento recibido:', JSON.stringify(event));
-    return { statusCode: 200, body: '¡Esta es la función Lambda!' };
+    return { statusCode: 200, body: '¡Esta es la  NUEVA función Lambda!' };
 };
 
 
